48e2476bcf05ce968dd6839ea9338e364f6514b9
Mardochee Dufffaut <119637751+MarDuff@users.noreply.github.com>
2023-09-02 23:45:29 -0400

    Add files via upload

---
+++ DSC_350 Assignment 1.2-2.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Python Basics functions
+
+# In[3]:
+
+
+#Display the text “Hello World! 
+print ("Hello Worl!")
+
+
+# In[9]:
+
+
+#Sum of two numbers
+a=5
+b=2.5
+(a+b)
+
+
+# In[8]:
+
+
+#Subtract a number from another number
+a=35
+b=10
+(a-b)
+
+
+# In[7]:
+
+
+#Multiply two numbers
+x=55
+y=3
+x*y
+
+
+# In[10]:
+
+
+#Divide between two numbers
+a=25
+b=3
+a/b
+
+
+# In[14]:
+
+
+#Concatenate two strings together
+Welcome= "Hello!"
+presentation= "My name is Marduff"
+Welcome + presentation
+
+
+# In[33]:
+
+
+#Create a list of 4 items 
+furniturelist=['Table','Bed','Sofa','Lamp']
+print(furniturelist)
+
+
+# In[36]:
+
+
+#Append an item to the list 
+furniturelist.append('TV')
+print(furniturelist)
+
+
+# In[38]:
+
+
+#Create a tuple with 4 items 
+tuple= ('Table','Bed','Sofa','Lamp')
+print(tuple)
+
+
+# In[ ]:
+
+
+
+
